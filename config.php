db5f7263161e5a1f5b560399752c2e38bebb7a27
Vaibhav Singh <singh12521vaibhav@gmail.com>
2026-05-31 10:52:25 +0530

    Update config.php

--- config.php
+++ config.php
@@ -7,7 +7,6 @@ $dbname = "lost_found_db";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 
-// Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
