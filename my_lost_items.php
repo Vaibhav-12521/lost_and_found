4b30e9da021615300467dae9ccaca0c425a9f9d7
Vaibhav Singh <singh12521vaibhav@gmail.com>
2026-06-20 23:21:15 +0530

    Update my_lost_items.php

--- my_lost_items.php
+++ my_lost_items.php
@@ -66,7 +66,6 @@ while ($row = $result->fetch_assoc()) {
     </style>
 </head>
 <body>
-    <!-- Navigation -->
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
         <div class="container">
             <a class="navbar-brand" href="index.php">
